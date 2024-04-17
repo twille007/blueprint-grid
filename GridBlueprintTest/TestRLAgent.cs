c054a84301c80151eb120fbef476ac1e7301c4ab
twille <thorben.w@posteo.net>
2024-04-17 16:29:59 +0200

    added TestSuite

---
+++ GridBlueprintTest/TestRLAgent.cs
@@ -0,0 +1,18 @@
+namespace GridBlueprintTest;
+
+public class TestRLAgent
+{
+
+    [Test]
+    public void LoadQTableFromCsvTest()
+    {
+        
+    }
+
+    [Test]
+    public void ExportQTableTest()
+    {
+        
+    }
+    
+}
